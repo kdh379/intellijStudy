856c717929eef346947c8087374237d7a93a168f
kdh379 <ehgus37625@naver.com>
2022-01-18 18:54:55 +0900

    Changes

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,13 @@
+package hello.hellospring.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class Member {
+
+    private Long id;
+    private String name;
+
+}
